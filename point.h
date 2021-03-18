78617db1a416a92b7a8c878e6700545ad42ee9ad
RICHARD <richardsaintcyr4@gmail.com>
2021-03-18 11:29:06 -0400

    l

---
+++ point.h
@@ -0,0 +1,15 @@
+#ifndef POINT_H
+#define POINT_H
+
+
+class point
+{
+public:
+    point(float x , float y );
+    float x1;
+    float y1;
+    point(float x , float y );
+    void deplace (float x , float y);
+};
+
+#endif // POINT_H
